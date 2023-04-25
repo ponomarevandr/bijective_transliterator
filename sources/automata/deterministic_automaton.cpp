bbbd8b1e04b0556a08cdb5d9bed54e3977bce597
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2023-04-25 15:31:32 +0300

    Deterministic automaton introduced

--- sources/automata/deterministic_automaton.cpp
+++ sources/automata/deterministic_automaton.cpp
@@ -1,1 +1,51 @@
 #include "deterministic_automaton.h"
+
+
+DeterministicAutomaton::Node::Node(size_t next_default, bool is_terminal):
+		is_terminal(is_terminal) {
+	for (size_t i = 0; i < CODE_SIZE; ++i) {
+		next[i] = next_default;
+	}
+}
+
+DeterministicAutomaton::DeterministicAutomaton(): current_state(0) {
+	nodes.emplace_back(0);
+}
+
+void DeterministicAutomaton::reset() {
+	current_state = 0;
+}
+
+size_t DeterministicAutomaton::getCurrentState() const {
+	return current_state;
+}
+
+bool DeterministicAutomaton::isCurrentTerminal() const {
+	return nodes[current_state].is_terminal;
+}
+
+WordCodes DeterministicAutomaton::doCurrentAction() const {
+	return nodes[current_state].action(rows_length);
+}
+
+void DeterministicAutomaton::step(Code code) {
+	current_state = nodes[current_state].next[code];
+}
+
+size_t DeterministicAutomaton::getRoot() const {
+	return 0;
+}
+
+size_t DeterministicAutomaton::addNode(size_t next_default) {
+	nodes.emplace_back(next_default);
+	return nodes.size() - 1;
+}
+
+void DeterministicAutomaton::setTerminalAndAction(size_t node, ActionFunction action) {
+	nodes[node].is_terminal = true;
+	nodes[node].action = action;
+}
+
+void DeterministicAutomaton::setNext(size_t node, Code code, size_t next) {
+	nodes[node].next[code] = next;
+}
--- sources/automata/deterministic_automaton.h
+++ sources/automata/deterministic_automaton.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "symbolic_analyser/encoding.h"
+
+#include <functional>
+
+
+class DeterministicAutomaton {
+public:
+	using ActionFunction = std::function<WordCodes(const std::vector<size_t>&)>;
+
+private:
+	struct Node {
+		size_t next[CODE_SIZE];
+		bool is_terminal;
+		ActionFunction action;
+
+		Node(size_t next_default, bool is_terminal = false);
+	};
+
+private:
+	std::vector<Node> nodes;
+	size_t current_state;
+	std::vector<size_t> rows_length;
+
+public:
+	DeterministicAutomaton();
+	void reset();
+	size_t getCurrentState() const;
+	bool isCurrentTerminal() const;
+	WordCodes doCurrentAction() const;
+	void step(Code code);
+	size_t getRoot() const;
+	size_t addNode(size_t next_default);
+	void setTerminalAndAction(size_t node, ActionFunction action);
+	void setNext(size_t node, Code code, size_t next);
+};
