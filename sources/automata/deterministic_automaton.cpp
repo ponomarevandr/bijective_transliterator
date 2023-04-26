44e8306551c72c5e37505346393e4718b189b94f
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2023-04-26 23:51:35 +0300

    Deterministic automaton upgraded

--- sources/automata/deterministic_automaton.cpp
+++ sources/automata/deterministic_automaton.cpp
@@ -1,9 +1,11 @@
 #include "deterministic_automaton.h"
 
+#include <queue>
+
 
 DeterministicAutomaton::Node::Node(size_t next_default, bool is_terminal):
 		is_terminal(is_terminal) {
-	for (size_t i = 0; i < CODE_SIZE; ++i) {
+	for (size_t i = 0; i <= CODE_SIZE; ++i) {
 		next[i] = next_default;
 	}
 }
@@ -14,6 +16,8 @@ DeterministicAutomaton::DeterministicAutomaton(): current_state(0) {
 
 void DeterministicAutomaton::reset() {
 	current_state = 0;
+	history.clear();
+	series_length.clear();
 }
 
 size_t DeterministicAutomaton::getCurrentState() const {
@@ -24,12 +28,35 @@ bool DeterministicAutomaton::isCurrentTerminal() const {
 	return nodes[current_state].is_terminal;
 }
 
+bool DeterministicAutomaton::isCurrentPerspective() const {
+	return nodes[current_state].is_perspective;
+}
+
 WordCodes DeterministicAutomaton::doCurrentAction() const {
-	return nodes[current_state].action(rows_length);
+	return nodes[current_state].action(series_length);
 }
 
 void DeterministicAutomaton::step(Code code) {
+	history.push_back(current_state);
 	current_state = nodes[current_state].next[code];
+	if (series_length.empty() || code != previous_code) {
+		series_length.push_back(1);
+		previous_code = code;
+	} else {
+		++series_length.back();
+	}
+}
+
+void DeterministicAutomaton::undoStep() {
+	if (history.empty())
+		return;
+	current_state = history.back();
+	history.pop_back();
+	if (series_length.back() == 0) {
+		series_length.pop_back();
+	} else {
+		--series_length.back();
+	}
 }
 
 size_t DeterministicAutomaton::getRoot() const {
@@ -48,4 +75,33 @@ void DeterministicAutomaton::setTerminalAndAction(size_t node, ActionFunction ac
 
 void DeterministicAutomaton::setNext(size_t node, Code code, size_t next) {
 	nodes[node].next[code] = next;
+}
+
+void DeterministicAutomaton::updateIsPerspective(size_t node) {
+	nodes[node].is_perspective = false;
+	std::queue<size_t> queue;
+	std::vector<int> is_visited(nodes.size(), false);
+	queue.push(node);
+	is_visited[node] = true;
+	while (!queue.empty()) {
+		size_t current = queue.front();
+		queue.pop();
+		if (nodes[current].is_terminal) {
+			nodes[node].is_perspective = true;
+			return;
+		}
+		for (size_t i = 0; i <= CODE_SIZE; ++i) {
+			size_t next = nodes[current].next[i];
+			if (is_visited[next])
+				continue;
+			queue.push(next);
+			is_visited[next] = true;
+		}
+	}
+}
+
+void DeterministicAutomaton::updateIsPerspective() {
+	for (size_t i = 0; i < nodes.size(); ++i) {
+		updateIsPerspective(i);
+	}
 }
--- sources/automata/deterministic_automaton.h
+++ sources/automata/deterministic_automaton.h
@@ -2,6 +2,7 @@
 
 #include "symbolic_analyser/encoding.h"
 
+#include <vector>
 #include <functional>
 
 
@@ -11,8 +12,9 @@ public:
 
 private:
 	struct Node {
-		size_t next[CODE_SIZE];
+		size_t next[CODE_SIZE + 1];
 		bool is_terminal;
+		bool is_perspective;
 		ActionFunction action;
 
 		Node(size_t next_default, bool is_terminal = false);
@@ -21,17 +23,25 @@ private:
 private:
 	std::vector<Node> nodes;
 	size_t current_state;
-	std::vector<size_t> rows_length;
+	std::vector<size_t> history;
+	std::vector<size_t> series_length;
+	Code previous_code;
+
+private:
+	void updateIsPerspective(size_t node);
 
 public:
 	DeterministicAutomaton();
 	void reset();
 	size_t getCurrentState() const;
 	bool isCurrentTerminal() const;
+	bool isCurrentPerspective() const;
 	WordCodes doCurrentAction() const;
 	void step(Code code);
+	void undoStep();
 	size_t getRoot() const;
 	size_t addNode(size_t next_default);
 	void setTerminalAndAction(size_t node, ActionFunction action);
 	void setNext(size_t node, Code code, size_t next);
+	void updateIsPerspective();
 };
